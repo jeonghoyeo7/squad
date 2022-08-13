10186a94da2d89f88cef1e750cce4403b718bddf
jeongho7yeo <jeongho7.yeo@gmail.com>
2022-08-14 00:33:51 +0900

    initial: boilerplate

---
+++ x/boost/client/testutil/helper.go
@@ -0,0 +1,29 @@
+package testutil
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/testutil"
+	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cosmosquad-labs/squad/v2/x/boost/client/cli"
+)
+
+var commonArgs = []string{
+	fmt.Sprintf("--%s=true", flags.FlagSkipConfirmation),
+	fmt.Sprintf("--%s=%s", flags.FlagBroadcastMode, flags.BroadcastBlock),
+	fmt.Sprintf("--%s=%s", flags.FlagFees, sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 10)).String()),
+}
+
+func MsgPlaceBid(clientCtx client.Context, from, poolId string, amount sdk.Coin, extraArgs ...string) (testutil.BufferWriter, error) {
+	args := append(append([]string{
+		poolId,
+		amount.String(),
+		fmt.Sprintf("--%s=%s", flags.FlagFrom, from),
+	}, commonArgs...), extraArgs...)
+
+	return clitestutil.ExecTestCLICmd(clientCtx, cli.NewPlaceBidCmd(), args)
+}
