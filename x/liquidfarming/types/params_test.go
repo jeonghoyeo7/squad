60b392c956b0c50d383786a1551891cd3ddbc58e
kogisin <jaybkim2@gmail.com>
2022-10-11 15:19:56 +0900

    test: add new params test cases

--- x/liquidfarming/types/params_test.go
+++ x/liquidfarming/types/params_test.go
@@ -2,9 +2,11 @@ package types_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 
 	"github.com/cosmosquad-labs/squad/v3/x/liquidfarming/types"
@@ -22,6 +24,56 @@ func TestParams_Validate(t *testing.T) {
 			"default params",
 			func(params *types.Params) {},
 			"",
+		},
+		{
+			"invalid pool id in liquid farm",
+			func(params *types.Params) {
+				params.LiquidFarms = []types.LiquidFarm{
+					types.NewLiquidFarm(0, sdk.ZeroInt(), sdk.ZeroInt(), sdk.ZeroDec()),
+				}
+			},
+			"invalid liquid farm: pool id must not be 0",
+		},
+		{
+			"invalid minimum farm amount in liquid farm",
+			func(params *types.Params) {
+				params.LiquidFarms = []types.LiquidFarm{
+					types.NewLiquidFarm(1, sdk.NewInt(-1), sdk.ZeroInt(), sdk.ZeroDec()),
+				}
+			},
+			"invalid liquid farm: minimum farm amount must be 0 or positive value: -1",
+		},
+		{
+			"invalid minimum bid amount in liquid farm",
+			func(params *types.Params) {
+				params.LiquidFarms = []types.LiquidFarm{
+					types.NewLiquidFarm(1, sdk.ZeroInt(), sdk.NewInt(-1), sdk.ZeroDec()),
+				}
+			},
+			"invalid liquid farm: minimum bid amount must be 0 or positive value: -1",
+		},
+		{
+			"invalid fee rate in liquid farm",
+			func(params *types.Params) {
+				params.LiquidFarms = []types.LiquidFarm{
+					types.NewLiquidFarm(1, sdk.ZeroInt(), sdk.ZeroInt(), sdk.NewDec(-1)),
+				}
+			},
+			"invalid liquid farm: fee rate must be 0 or positive value: -1.000000000000000000",
+		},
+		{
+			"invalid rewards auction duration",
+			func(params *types.Params) {
+				params.RewardsAuctionDuration = time.Duration(0)
+			},
+			"invalid rewards auction duration: 0 must be positive value",
+		},
+		{
+			"invalid fee collector",
+			func(params *types.Params) {
+				params.FeeCollector = "invalidaddr"
+			},
+			"invalid fee collector address: invalidaddr",
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
